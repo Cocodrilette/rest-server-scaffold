644fc825aa285634a46a392109a9f519af54fec2
Juan <jcogollo@unal.edu.co>
2022-09-16 11:30:24 -0500

    imports bugs fixed

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,9 @@
 const JWTValidator = require("../middlewares/jwtValidator");
 const requestFieldValidator = require("../middlewares/requestFieldsValidator");
-const haveRole = require("../middlewares/rolesValidators");
+const roleValidator = require("../middlewares/rolesValidators");
 
 module.exports = {
   JWTValidator,
   ...requestFieldValidator,
-  ...haveRole,
+  ...roleValidator,
 };
